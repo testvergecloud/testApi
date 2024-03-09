52517077951ee9bdd6b483c4f62305e048d7296a
mehrdad <jafari.ceai@gmail.com>
2024-03-10 00:02:49 +0330

    change config method

--- app/tooling/cdn-admin/main.go
+++ app/tooling/cdn-admin/main.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -11,95 +10,59 @@ import (
 	"github.com/testvergecloud/testApi/app/tooling/cdn-admin/commands"
 	"github.com/testvergecloud/testApi/foundation/config"
 	"github.com/testvergecloud/testApi/foundation/logger"
+	"go.uber.org/fx"
 
 	"github.com/google/uuid"
 )
 
-var build = "develop"
-
-// type config struct {
-// 	conf.Version
-// 	Args conf.Args
-// 	DB   struct {
-// 		User         string `conf:"default:postgres"`
-// 		Password     string `conf:"default:postgres,mask"`
-// 		HostPort     string `conf:"default:database-service.cdn-system.svc.cluster.local"`
-// 		Name         string `conf:"default:postgres"`
-// 		MaxIdleConns int    `conf:"default:2"`
-// 		MaxOpenConns int    `conf:"default:0"`
-// 		DisableTLS   bool   `conf:"default:true"`
-// 	}
-// 	Auth struct {
-// 		KeysFolder string `conf:"default:zarf/keys/"`
-// 		DefaultKID string `conf:"default:54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"`
-// 	}
-// }
-
 func main() {
-	log := logger.New(io.Discard, logger.LevelInfo, "ADMIN", func(context.Context) string { return "00000000-0000-0000-0000-000000000000" })
+	// log := logger.New(io.Discard, logger.LevelInfo, "ADMIN", func(context.Context) string { return "00000000-0000-0000-0000-000000000000" })
 
-	if err := run(log); err != nil {
-		if !errors.Is(err, commands.ErrHelp) {
-			fmt.Println("msg", err)
-		}
-		os.Exit(1)
-	}
+	// if err := run(log); err != nil {
+	// 	if !errors.Is(err, commands.ErrHelp) {
+	// 		fmt.Println("msg", err)
+	// 	}
+	// 	os.Exit(1)
+	// }
+	ServerModule := fx.Options(
+		fx.Provide(loadConfig),
+		fx.Provide(initializeLogger),
+		fx.Invoke(run),
+	)
+	fx.New(ServerModule).Run()
 }
 
-func run(log *logger.Logger) error {
-	// cfg := config{
-	// 	Version: conf.Version{
-	// 		Build: build,
-	// 		Desc:  "copyright information here",
-	// 	},
-	// }
-
-	const prefix = "CDN"
-	// help, err := conf.Parse(prefix, &cfg)
-	// if err != nil {
-	// 	if errors.Is(err, conf.ErrHelpWanted) {
-	// 		fmt.Println(help)
-	// 		return nil
-	// 	}
-
-	// 	out, err := conf.String(&cfg)
-	// 	if err != nil {
-	// 		return fmt.Errorf("generating config for output: %w", err)
-	// 	}
-	// 	log.Info(context.Background(), "startup", "config", out)
-
-	// 	return fmt.Errorf("parsing config: %w", err)
-	// }
-
-	return processCommands(log)
-}
-
-// processCommands handles the execution of the commands specified on
+// run handles the execution of the commands specified on
 // the command line.
-func processCommands(log *logger.Logger) error {
-	cfg := LoadConfig()
+func run(cfg *config.Config, log *logger.Logger) {
+	ctx := context.Background()
 	switch os.Args[0] {
 	case "domain":
 		if err := commands.Domain(os.Args[1]); err != nil {
-			return fmt.Errorf("adding domain: %w", err)
+			log.Error(ctx, "adding domain: ", err)
+			return
 		}
 
 	case "migrate":
 		if err := commands.Migrate(cfg); err != nil {
-			return fmt.Errorf("migrating database: %w", err)
+			log.Error(ctx, "migrating database: ", err)
+			return
 		}
 
 	case "seed":
 		if err := commands.Seed(cfg); err != nil {
-			return fmt.Errorf("seeding database: %w", err)
+			log.Error(ctx, "seeding database: ", err)
+			return
 		}
 
 	case "migrate-seed":
 		if err := commands.Migrate(cfg); err != nil {
-			return fmt.Errorf("migrating database: %w", err)
+			log.Error(ctx, "migrating database: ", err)
+			return
 		}
 		if err := commands.Seed(cfg); err != nil {
-			return fmt.Errorf("seeding database: %w", err)
+			log.Error(ctx, "seeding database: ", err)
+			return
 		}
 
 	case "useradd":
@@ -107,32 +70,37 @@ func processCommands(log *logger.Logger) error {
 		email := os.Args[2]
 		password := os.Args[3]
 		if err := commands.UserAdd(log, cfg, name, email, password); err != nil {
-			return fmt.Errorf("adding user: %w", err)
+			log.Error(ctx, "adding user: ", err)
+			return
 		}
 
 	case "users":
 		pageNumber := os.Args[1]
 		rowsPerPage := os.Args[2]
 		if err := commands.Users(log, cfg, pageNumber, rowsPerPage); err != nil {
-			return fmt.Errorf("getting users: %w", err)
+			log.Error(ctx, "getting users: ", err)
+			return
 		}
 
 	case "genkey":
 		if err := commands.GenKey(); err != nil {
-			return fmt.Errorf("key generation: %w", err)
+			log.Error(ctx, "key generation: ", err)
+			return
 		}
 
 	case "gentoken":
 		userID, err := uuid.Parse(os.Args[1])
 		if err != nil {
-			return fmt.Errorf("generating token: %w", err)
+			log.Error(ctx, "generating token: ", err)
+			return
 		}
 		kid := os.Args[2]
 		if kid == "" {
 			kid = cfg.DefaultKID
 		}
 		if err := commands.GenToken(log, cfg, cfg.KeysFolder, userID, kid); err != nil {
-			return fmt.Errorf("generating token: %w", err)
+			log.Error(ctx, "generating token: ", err)
+			return
 		}
 
 	default:
@@ -144,16 +112,21 @@ func processCommands(log *logger.Logger) error {
 		fmt.Println("genkey:     generate a set of private/public key files")
 		fmt.Println("gentoken:   generate a JWT for a user with claims")
 		fmt.Println("provide a command to get more help.")
-		return commands.ErrHelp
+		log.Error(ctx, "commands.ErrHelp: ", commands.ErrHelp)
+		return
 	}
-
-	return nil
 }
 
-func LoadConfig() *config.Config {
-	c, err := config.LoadConfig(".")
+func loadConfig(log *logger.Logger) (*config.Config, error) {
+	c, err := config.LoadConfig("./foundation/env/cdn/")
 	if err != nil {
-		fmt.Errorf("loading config: %w", err)
+		return nil, err
 	}
-	return c
+
+	log.Info(context.Background(), "config load successfully", "config: ", c)
+	return c, nil
+}
+
+func initializeLogger() *logger.Logger {
+	return logger.New(io.Discard, logger.LevelInfo, "ADMIN", func(context.Context) string { return "00000000-0000-0000-0000-000000000000" })
 }
